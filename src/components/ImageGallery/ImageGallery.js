a8bd0cb9cb32f84aa07d14bc965b9929ef6d1c0b
Aleksey Dibikov <dibikovaleksey@gmail.com>
2021-10-17 00:21:07 +0300

    adds components folder and file

---
+++ src/components/ImageGallery/ImageGallery.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ImageGallery extends Component {
+  render() {
+    return (
+      <ul className="ImageGallery">
+        {/* <!-- Набор <li> с изображениями --> */}
+      </ul>
+    );
+  }
+}
+
+export default ImageGallery;
